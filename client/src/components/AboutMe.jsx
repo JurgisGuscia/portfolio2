c05b0829fffb83f7fb354329d7215297b599a54e
JurgisGuscia <j.d.guscia@gmail.com>
2023-08-26 23:34:54 +0300

    fixes

--- client/src/components/AboutMe.jsx
+++ client/src/components/AboutMe.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from "react";
-// import { gsap } from "gsap";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
-// gsap.registerPlugin(ScrollTrigger);
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 export default function AboutMe() {
   const aboutMeHeaderRef = useRef();
